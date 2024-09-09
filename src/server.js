32695ab340df26c990214aa746605aba29a9a359
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-09-09 18:02:01 -0400

    implemented logic for upvote

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 import express from "express";
 
+let articlesInfo = [
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  { name: "learn-react", 
+    upvotes: 0 },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
@@ -7,13 +20,19 @@ app.use(express.json());
 
 app.put("/api/articles/:name/upvote", (req, res) => {
   const { name } = req.params;
-  const { upvotes } = req.body;
 
-  articlesInfo[name].upvotes = upvotes;
-  res.status(200).send(articlesInfo[name]);
+  const article = articlesInfo.find(a=> a.name === name);
+
+  if (article){
+    article.upvotes += 1;
+    res.status(200).send(`${name} now has ${article.upvotes} upvotes!`);
+  }
+    else{
+        res.status(404).send("Article not found");
+    }
 });
 
 
-app.listen(8000, () => {
+
   console.log("server is running on port 8000");
-});
+
